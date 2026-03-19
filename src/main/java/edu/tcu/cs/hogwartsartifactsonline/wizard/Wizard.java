49e1feeeb83c1f47aff6b3f693bc7b79c7ba11bf
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2026-03-19 18:49:21 -0500

    Completed video 23

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/Wizard.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/Wizard.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -18,7 +19,7 @@ public class Wizard implements Serializable {
     private String name;
 
     @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "owner")
-    private List<Artifact> artifacts;
+    private List<Artifact> artifacts = new ArrayList<>();
 
     public Wizard() {
     }
@@ -45,5 +46,14 @@ public class Wizard implements Serializable {
 
     public void setArtifacts(List<Artifact> artifacts) {
         this.artifacts = artifacts;
+    }
+
+    public void addArtifact(Artifact artifact) {
+        artifacts.add(artifact);
+        artifact.setOwner(this);
+    }
+
+    public Integer getNumberOfArtifacts() {
+        return this.artifacts.size();
     }
 }
