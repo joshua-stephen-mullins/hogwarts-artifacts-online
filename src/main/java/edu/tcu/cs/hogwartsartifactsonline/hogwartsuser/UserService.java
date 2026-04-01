16c31b767161e1b59f531976db68cd7a29e4ffaf
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2026-04-01 15:11:55 -0500

    Completed up to video 42! User authentication and authorization done, Close #5

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserService.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserService.java
@@ -2,20 +2,27 @@ package edu.tcu.cs.hogwartsartifactsonline.hogwartsuser;
 
 import edu.tcu.cs.hogwartsartifactsonline.system.exception.ObjectNotFoundException;
 import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @Transactional
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    private PasswordEncoder passwordEncoder;
 
 
-    public UserService(UserRepository userRepository) {
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
@@ -31,6 +38,8 @@ public class UserService {
 
 
     public HogwartsUser save(HogwartsUser newHogwartsUser) {
+        newHogwartsUser.setPassword(this.passwordEncoder.encode(newHogwartsUser.getPassword()));
+
         return this.userRepository.save(newHogwartsUser);
     }
 
@@ -58,4 +67,10 @@ public class UserService {
         this.userRepository.deleteById(userId);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username)
+                .map(MyUserPrincipal::new)
+                .orElseThrow(() -> new UsernameNotFoundException("username " + username + " is not found."));
+    }
 }
